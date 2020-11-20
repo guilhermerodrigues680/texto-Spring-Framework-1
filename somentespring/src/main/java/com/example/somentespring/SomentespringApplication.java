7b185f302757720b36682ec25a38358d8bf3be79
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2020-11-20 10:10:46 -0300

    Extraido projeto Somente Spring

---
+++ somentespring/src/main/java/com/example/somentespring/SomentespringApplication.java
@@ -0,0 +1,13 @@
+package com.example.somentespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomentespringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomentespringApplication.class, args);
+	}
+
+}
---
+++ somentespring/src/test/java/com/example/somentespring/SomentespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.somentespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomentespringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
